1f02681300286b9c0f4af6d9eb0299e32503bb38
Ignjatije Izipac <ignjatije.i@gmail.com>
2026-04-10 14:48:39 +0200

    add wp-content rewrite for images

--- next.config.ts
+++ next.config.ts
@@ -3,24 +3,8 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/wp-json/:path*',
-        destination: 'http://195.35.49.191/wp-json/:path*',
-        has: [
-          {
-            type: 'header',
-            key: 'host',
-          },
-        ],
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: '/wp-json/:path*',
-        headers: [
-          { key: 'Host', value: 'doorgatesistem.com' },
-        ],
+        source: '/wp-content/:path*',
+        destination: 'https://api.doorgatesistem.com/wp-content/:path*',
       },
     ];
   },
@@ -40,19 +24,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: '*.doorgatesistem.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'api.doorgatesistem.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
         port: '',
         pathname: '/**',
       },
