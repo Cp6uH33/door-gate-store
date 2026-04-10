006ae5e972ef3916e1518caefa3545263c911892
Ignjatije Izipac <ignjatije.i@gmail.com>
2026-04-10 14:43:58 +0200

    add api subdomain to image patterns

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'api.doorgatesistem.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'via.placeholder.com',
         port: '',
         pathname: '/**',
